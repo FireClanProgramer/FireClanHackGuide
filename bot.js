c3e649e6f20b1653abc9defccee724f15c7a3796
FireClanProgramer <42547710+FireClanProgramer@users.noreply.github.com>
2018-08-20 18:26:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,18 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-
+client.on('message', message => {
+   if (message.content === "الامر") {
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+  .setThumbnail(message.author.avatarURL)
+  .addField("Name:",`${message.author.username}`, true)
+  .addField('Discrim:',"#" +  message.author.discriminator, true)
+  .addField("ID:", message.author.id, true)
+  .addField("Create At:", message.author.createdAt, true)
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
 client.login(process.env.BOT_TOKEN);
