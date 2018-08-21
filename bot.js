f917584acb85f8eb15f83a5e582fe2268fb33417
FireClanProgramer <42547710+FireClanProgramer@users.noreply.github.com>
2018-08-21 23:37:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,9 +54,11 @@ __~~Done Bot~~__ By: ᵀᴼᴾᔆ | xiYoussef_.#6363
              Admin Commands
 ╚[❖════════════❖]╝
 
+ ❖ *ban  <mention > ➾ ban member from server
+
  ❖ *kick <mention > ➾ kick member from server
 
- ❖ *clear ➾ clear chat ( not ready yet fixing)
+ ❖ *clear ➾ clear chat
  
  ❖ *mute < mention > ➾ mute member
 
@@ -83,15 +85,13 @@ __~~Done Bot~~__ By: ᵀᴼᴾᔆ | xiYoussef_.#6363
 
 ❖ *uptime ➾ to see uptime
 
-❖ *invs ➾ server invite link
-
 ❖ *own ➾ bot owner
 
 ❖ *id ➾ your id
 
 ❖ *avatar ➾ your avatar account
 
-❖ *help-en ➾ help by arabic
+❖ *help-ar ➾ help by arabic
 
 ❖ *help-en ➾ help by english
 
@@ -122,6 +122,212 @@ bot invite link: https://discordapp.com/oauth2/authorize?client_id=4804786277297
 
 `);
 
+    }
+});
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc1') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+client.on('message', message => {
+if (message.content.startsWith("ban")) {
+    var mention = message.mentions.members.first();
+    if(!mention) return message.channel.send("يجب منشن العضو");
+
+    mention.ban("By: " + message.author.tag);
+    
+    message.channel.send("تم أعطاء باند الى : " + mention.tag);
+};
+});
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها 👌```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+
+3.client.on('message', message => {
+
+
+4.if (message.content.startsWith(prefix+"ct")) {
+
+
+5.    var args = message.content.split(" ").slice(1);
+
+
+6.    var argrst = args.join(' ');
+
+
+7.                message.guild.createChannel(`${argrst}`, 'text')
+
+
+8.      }
+
+
+9.});
+
+3.client.on('message', message => {
+
+
+4.if (message.content.startsWith(prefix+"invite")) {
+
+
+5.    var args = message.content.split(" ").slice(1);
+
+
+6.    var argrst = args.join(' ');
+
+
+7.                message.guild.createChannel(`${argrst}`, 'text')
+
+
+8.      }
+
+
+9.});
+
+1.client.on('message', message => {
+
+
+2.if (message.content.startsWith(prefix+"cv")) {
+
+
+3.    var args = message.content.split(" ").slice(1);
+
+
+4.    var argrst = args.join(' ');
+
+
+5.                message.guild.createChannel(`${argrst}`,'voice')
+
+
+6.          
+
+
+7.        }
+
+
+8.});
+client.on('message', function(message) {
+    if(message.content.startsWith(prefix + 'roll')) {
+        let args = message.content.split(" ").slice(1);
+        if (!args[0]) {
+            message.channel.send('**حط رقم معين يتم السحب منه**');
+            return;
+            }
+    message.channel.send(Math.floor(Math.random() * args.join(' ')));
+            if (!args[0]) {
+          message.edit('1')
+          return;
+        }
+    }
+});
+client.on('message', message => {
+    var prefix = "*"
+var args = message.content.split(" ").slice(1);    
+if(message.content.startsWith(prefix + 'id')) {
+var year = message.author.createdAt.getFullYear()
+var month = message.author.createdAt.getMonth()
+var day = message.author.createdAt.getDate()
+var men = message.mentions.users.first();  
+let args = message.content.split(' ').slice(1).join(' ');
+if (args == '') {
+var z = message.author;
+}else {
+var z = message.mentions.users.first();
+}
+
+let d = z.createdAt;          
+let n = d.toLocaleString();   
+let x;                       
+let y;                        
+
+if (z.presence.game !== null) {
+y = `${z.presence.game.name}`;
+} else {
+y = "No Playing... |💤.";
+}
+if (z.bot) {
+var w = 'بوت';
+}else {
+var w = 'عضو';
+}
+let embed = new Discord.RichEmbed()
+.setColor("#502faf")
+.addField('🔱| اسمك:',`**<@` + `${z.id}` + `>**`, true)
+.addField('🛡| ايدي:', "**"+ `${z.id}` +"**",true)
+.addField('♨| Playing:','**'+y+'**' , true)
+.addField('🤖| نوع حسابك:',"**"+ w + "**",true)    
+.addField('📛| الكود حق حسابك:',"**#" +  `${z.discriminator}**`,true)
+.addField('**التاريح الذي انشئ فيه حسابك | 📆 **: ' ,year + "-"+ month +"-"+ day)    
+.addField("**تاريخ دخولك للسيرفر| ⌚   :**", message.member.joinedAt.toLocaleString())    
+
+.addField('**⌚ | تاريخ انشاء حسابك الكامل:**', message.author.createdAt.toLocaleString())
+.addField("**اخر رسالة لك | 💬  :**", message.author.lastMessage)            
+
+.setThumbnail(`${z.avatarURL}`)
+.setFooter(message.author.username, message.author.avatarURL)
+
+message.channel.send({embed});
+    if (!message) return message.reply('**ضع المينشان بشكل صحيح  ❌ **').catch(console.error);
+
+}
+
+});
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+            if (message.content.startsWith(prefix + "ping")) {
+        message.channel.sendMessage(':ping_pong: Pong! In `' + `${client.ping}` + ' ms`');
+    }
+});
+client.on('message', message => {
+    if (message.content.startsWith("^avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
     }
 });
 client.login(process.env.BOT_TOKEN);
