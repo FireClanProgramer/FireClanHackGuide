f31c4895a921db0a5c6dcb35c6ab6c2cf0e296ef
FireClanProgramer <42547710+FireClanProgramer@users.noreply.github.com>
2018-08-21 17:40:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '-'
+const prefix = '.'
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Made By xiYoussef`,"http://twitch.tv/S-F")
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -27,67 +27,7 @@ client.user.setGame(`Made By xiYoussef`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
-client.on('message',function(message) {
-  if (message.author.bot) return;
-
-
-                  if(!message.channel.guild) return;
-
-                    if (message.content === prefix + "members") {
- const embed = new Discord.RichEmbed()
-
-    .setDescription(`**Members info ✨
- اونلاين:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
-  حاله حمراء       ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
-  حاله صفراء:     ${message.guild.members.filter(m=>m.presence.status == 'idle').size}
-  الاوفلاين:     ${message.guild.members.filter(m=>m.presence.status == 'offline').size}
-   عدد الاعضاء:  ${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size}
- البوتات: ${message.guild.members.filter(m=>m.user.bot).size} **`)
-         message.channel.send({embed});
-
-    }
-      });
 client.on('message', message => {
-    if (message.content.startsWith("-تهكير")) {
-      if (message.author.bot) return
-           message.delete();
-             let args = message.content.split(' ').slice(1);
-                   let virusname = args.join(' ');
-                 if (virusname < 1) {
-                     return message.channel.send("``اسم الي تبي تهكره``");
-                                     }
-                 message.channel.send({embed: new Discord.RichEmbed().setTitle('Loading ' + virusname + "...").setColor(0xFF0000)}).then(function(m) {
-             setTimeout(function() {
-               m.edit({embed: new Discord.RichEmbed().setTitle('[' + virusname + ']: Loading Discord Virus [▓ ] 1%').setColor(0xFF0000)})
-             }, 1000)
-            setTimeout(function() {
-               m.edit({embed: new Discord.RichEmbed().setTitle('[' + virusname + ']: Loading Discord Virus [▓▓▓▓] 25%').setColor(0xFF0000)})
-             }, 2000)
-           setTimeout(function() {     
-               m.edit({embed: new Discord.RichEmbed().setTitle('[' + virusname + ']: Loading Discord Virus [▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ] 100%').setColor(0xFF0000)})
-             }, 3000)
-                setTimeout(function() {
-               m.edit({embed: new Discord.RichEmbed().setTitle('[' + virusname + ']: Uploaded! Initiating explosion in 1...').setColor(0xFF0000)})
-             }, 4000)
-              setTimeout(function() {
-               m.delete()
-           }, 5000)
-             setTimeout(function() {
-               message.channel.send('تم تهكيرك')
-           }, 6000)
-           });
-         }
- });
-client.on('message' , message => {
-  var prefix = "-";
-  if(message.author.bot) return;
-  if(message.content.startsWith(prefix + "ping")) {
- message.channel.send('Pong...').then((msg) => {
-      msg.edit(`\`\`\`javascript\nTime taken: ${msg.createdTimestamp - message.createdTimestamp} ms.\nDiscord API: ${Math.round(client.ping)} ms.\`\`\``);
- })
-  }  
- });
-lient.on('message', message => {
     if (message.author.bot) return;
      if (message.content === prefix + "help-en") {
 		 message.channel.send('**The Message Was Sent On Private**');
@@ -105,6 +45,7 @@ __~~Done Bot~~__ By: ᵀᴼᴾᔆ | xiYoussef_.#6363
 ╱┃┃┃┃╭╮┃╭╮┫┃━┫┃╭━╮┃╭╮┃┃
 ╭╯╰╯┃╰╯┃┃┃┃┃━┫┃╰━╯┃╰╯┃╰╮
 ╰━━━┻━━┻╯╰┻━━╯╰━━━┻━━┻━╯
+
 ╔[❖════════════❖]╗
              Prefix = ' - '
 ╚[❖════════════❖]╝
